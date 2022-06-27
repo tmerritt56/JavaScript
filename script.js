0179e63c7809e9f6489bb7658e82c538d4c46257
tmerritt56 <104723943+tmerritt56@users.noreply.github.com>
2022-06-26 20:25:21 -0500

    created first window.prompt asking, length of character

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 // Assignment Code
+// button
 var generateBtn = document.querySelector("#generate");
+var value = [8,9,10,11,12,13];
+
 
 // Write password to the #password input
+function generatePassword () {
+  console.log ("You Clicked the button");
+// 1. Prompt for user to ask password Criteria
+  var length= window.prompt ("How many characters would you like for your password?")
+// brings up prompt!
+//  a. Password length between 8-128
+//  b.Lower, Uppercase, numbers, special characters
+// 2. After the prompt, validate input and 1 character type should be selected 
+// 3. When all prompts answered, generate password with the selected criteria
+
+
+
+
+
+// 4. write password to page. 
+  return "Generated Password, will go here.";
+}
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Add event listener to generate buttoN
+// when button clicked, runs function
+generateBtn.addEventListener("click", writePassword); 
