7cba6b988d495407053159a16131e56be43cd84b
tmerritt56 <104723943+tmerritt56@users.noreply.github.com>
2022-06-26 21:07:01 -0500

    added variables for the prompts.

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 // Assignment Code
 // button
 var generateBtn = document.querySelector("#generate");
-var value = [8,9,10,11,12,13];
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var numbericalChar = "0123456789";
+var specialChar = "!@#$%^&*()<>/{}[]_~`:;',./\|.+-";
 
 
 // Write password to the #password input
 function generatePassword () {
   console.log ("You Clicked the button");
-// 1. Prompt for user to ask password Criteria
-  var length= window.prompt ("How many characters would you like for your password?")
+// 1. Prompt for user vto ask password Criteria
+ var choice =  window.prompt ("How many characters would you like for your password?")
 // brings up prompt!
-//  a. Password length between 8-128
+if (!choice) {
+  return;
+}
+// returns undefined
+
+  // a. Password length between 8-128
 //  b.Lower, Uppercase, numbers, special characters
 // 2. After the prompt, validate input and 1 character type should be selected 
 // 3. When all prompts answered, generate password with the selected criteria
