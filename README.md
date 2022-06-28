f6f94edb6cd2e77547b5a8131273aee0cec741a6
tmerritt56 <104723943+tmerritt56@users.noreply.github.com>
2022-06-27 22:10:53 -0500

    Added more code to prompts to better the flow, added extra conditional code to make sure at least one criteria was chosen.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # JavaScript
+
+## Description
+- This was created to randomly generate a password based on the criteria you choose. 
--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var numbericalChar = "0123456789";
 var specialChar = "!@#$%^&*()<>/{}[]_~`:;',./\|.+-";
 
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -14,40 +15,33 @@ function writePassword() {
   // Write password to the #password input
 function generatePassword () {
   console.log ("You Clicked the button");
-// 1. Prompt for user vto ask password Criteria
-var passwordChars = "";
+// 1. Prompt for user to ask password Criteria
 // brings up prompt!
- var length =  window.prompt ("How many characters would you like for your password? It needs to be between 8 and 128. Thanks <3.");
+ var length =  parseInt(window.prompt ("How many characters would you like for your password? It needs to be between 8 and 128. Thanks <3."));
 // a. Password length between 8-128
-length = parseInt (length);
- if (length < 8) {
+if (!length) {
+  alert("Password must have a length.")
+}else if (length < 8) {
   alert("Password must contain more than 7 characters.");
   return "";
  } else if (length > 128) {
   alert("Password must be less than 128 characters.")
- }
-
+ } else { var littleLetters = confirm("Would you like to add some little letters?");
  //  b.Lower, Uppercase, numbers, special characters
- var littleLetters = confirm("Would you like to add some little letters?");
-
-if(littleLetters){
-  passwordChars += lowerCase;
-}
-
-var bigLetters = confirm("Would you like to add BIG LETTERS?");
- if (bigLetters) {
-  password += upperCase;
+ var bigLetters = confirm("Would you like to add BIG LETTERS?");
+ var digits = confirm("Would you like to add some digits?");
+  var prettyIcons = confirm("Wanna add some pretty icons(special characters)?");
+ }  if (!littleLetters, !bigLetters, !digits, !prettyIcons) {
+  choices = alert("Must choose at least one password criteria. <3")
  }
+
+ }
+
 // 2. After the prompt, validate input and 1 character type should be selected 
 // 3. When all prompts answered, generate password with the selected criteria
- 
-
-
 
 
 // 4. write password to page. 
- return "Generated Password, will go here.";
-}
-// Add event listener to generate buttoN
+// Add event listener to generate button
 // when button clicked, runs function
 generateBtn.addEventListener("click", writePassword); 
